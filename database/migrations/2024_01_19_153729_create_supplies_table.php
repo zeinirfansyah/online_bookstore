48aafb0aea7de2e906355f865d12b25de19280d2
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-01-19 22:46:22 +0700

    add: create_supplies_table migration

---
+++ database/migrations/2024_01_19_153729_create_supplies_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('supplies', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('supplier_id')->unsigned();
+            $table->date('supply_date');
+            $table->timestamps();
+            $table->foreign('supplier_id')->references('id')->on('suppliers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('supplies');
+    }
+};

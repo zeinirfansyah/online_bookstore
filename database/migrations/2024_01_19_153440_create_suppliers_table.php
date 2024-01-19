a7a2ca3337f9aab01852370b535ed4711793e2bf
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-01-19 22:36:20 +0700

    add: create_suppliers_table migration

---
+++ database/migrations/2024_01_19_153440_create_suppliers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->string('supplier_name');
+            $table->string('supplier_address');
+            $table->string('supplier_phone');
+            $table->string('supplier_email')->nullable();
+            $table->string('supplier_website')->nullable();
+            $table->string('supplier_description')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('suppliers');
+    }
+};

c8460876d181e0578c05fba8d214b51452e56c84
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-01-24 20:50:59 +0700

    update: sidebar style

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -1,14 +1,8 @@
 <!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-light-primary elevation-0">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img
-            src="{{ asset('/AdminLTE/dist/img/AdminLTELogo.png') }}"
-            alt="AdminLTE Logo"
-            class="brand-image img-circle elevation-3"
-            style="opacity: 0.8"
-        />
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light">Online Bookstore</span>
     </a>
 
     <!-- Sidebar -->
@@ -17,13 +11,13 @@
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
                 <img
-                    src="{{ asset('/AdminLTE/dist/img/user2-160x160') }}.jpg"
+                    src="https://zeinirfansyah.me/assets/foto4-fab19a53.png"
                     class="img-circle elevation-2"
                     alt="User Image"
                 />
             </div>
             <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
+                <a href="#" class="d-block">Zein Irfansyah</a>
             </div>
         </div>
 

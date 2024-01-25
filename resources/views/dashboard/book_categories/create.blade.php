5e489a593ad5dd540dccad6e13264532dba84ec2
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-01-26 01:38:38 +0700

    update: book_category input form

--- resources/views/dashboard/book_categories/create.blade.php
+++ resources/views/dashboard/book_categories/create.blade.php
@@ -1,38 +1,71 @@
 @extends('dashboard.layouts.master') @section('content')
-<h1>Create New Book</h1>
+  <div class="content-wrapper">
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Book Categories</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item">Home</li>
+              <li class="breadcrumb-item">Book Categories</li>
+              <li class="breadcrumb-item active">Create Book</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="content">
+      <div class="container-fluid">
+        @if ($errors->any())
+          <div>
+            <ul>
+              @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+              @endforeach
+            </ul>
+          </div>
+        @endif
 
-{{-- Display validation errors --}}
-@if ($errors->any())
-<div>
-    <ul>
-        @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-</div>
-@endif
 
-{{-- Form for creating a new book --}}
-<form
-    action="{{ route('book_categories.store') }}"
-    method="POST"
-    enctype="multipart/form-data"
->
-    @csrf
+        <div class="row">
+          <div class="col">
+            <div class="card p-3">
+              <form action="{{ route('book_categories.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
 
-    <label for="title">Category Name:</label>
-    <input
-        type="text"
-        name="category_name"
-        value="{{ old('category_name') }}"
-        required
-    />
+                <label for="title" class="form-label">Category Name:</label>
+                <input type="text" name="category_name" value="{{ old('category_name') }}" required
+                  class="form-control" />
 
-    <label for="author">Category Description:</label>
-    <textarea name="category_description" required>{{
-        old("category_description")
-    }}</textarea>
+                <label for="author" class="form-label">Category Description:</label>
+                <textarea name="category_description" class="form-control">{{ old('category_description') }}</textarea>
 
-    <button type="submit">Create Book Category</button>
-</form>
+                <button type="submit" class="btn btn-primary mt-2">
+                  Create Book Category
+                </button>
+              </form>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card p-3">
+
+              Available book categories:
+              <ul>
+                @foreach ($book_categories as $book_category)
+                  <li>
+                    {{ $book_category->category_name }}
+                  </li>
+                @endforeach
+              </ul>
+              {{ $book_categories->links('pagination::bootstrap-4') }}
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
 @endsection
--- resources/views/dashboard/book_categories/index.blade.php
+++ resources/views/dashboard/book_categories/index.blade.php
@@ -1,125 +1,102 @@
 @extends('dashboard.layouts.master') @section('content')
-<div class="content-wrapper">
+  <div class="content-wrapper">
     <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0">Book Categories</h1>
-                </div>
-                <!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item">
-                            <a href="#">Home</a>
-                        </li>
-                        <li class="breadcrumb-item active">Book Categories</li>
-                    </ol>
-                </div>
-                <!-- /.col -->
-            </div>
-            <!-- /.row -->
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Book Categories</h1>
+          </div>
+          <!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item">Home</li>
+              <li class="breadcrumb-item active">Book Categories</li>
+            </ol>
+          </div>
+          <!-- /.col -->
         </div>
-        <!-- /.container-fluid -->
+        <!-- /.row -->
+      </div>
+      <!-- /.container-fluid -->
     </div>
     <div class="content">
-        <div class="container-fluid">
-          <div>@includeif('dashboard.layouts.stat_cards')</div>
-            <!-- filter and search -->
-            <div class="row">
-                <div class="col-md-12">
-                   <div class="container-fluid">
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">Filter and Search</h3>
+      <div class="container-fluid">
+        <div>@includeif('dashboard.layouts.stat_cards')</div>
+        <!-- filter and search -->
+        <div class="row">
+          <div class="col-md-12">
+            <div class="container-fluid">
+              <div class="card">
+                <div class="card-header">
+                  <a href="{{ route('book_categories.create') }}" class="btn btn-primary">Add Category</a>
 
-                            <div class="card-tools">
-                                <div
-                                    class="input-group input-group-sm"
-                                    style="width: 150px"
-                                >
-                                    <input
-                                        type="text"
-                                        name="table_search"
-                                        class="form-control float-right"
-                                        placeholder="Search"
-                                    />
+                  <div class="card-tools">
+                    <div class="input-group input-group-sm" style="width: 150px">
+                      <input type="text" name="table_search" class="form-control float-right" placeholder="Search" />
 
-                                    <div class="input-group-append">
-                                        <button
-                                            type="submit"
-                                            class="btn btn-default"
-                                        >
-                                            <i class="fas fa-search"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                      <div class="input-group-append">
+                        <button type="submit" class="btn btn-default">
+                          <i class="fas fa-search"></i>
+                        </button>
+                      </div>
                     </div>
-                   </div>
+                  </div>
                 </div>
+              </div>
             </div>
-
-            <!-- Main row -->
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="container-fluid">
-                        <div class="card">
-                            <div class="table-responsive">
-                                <table class="table table-bordered">
-                                    <thead>
-                                        <tr>
-                                            <th>Category Name</th>
-                                            <th>Category Description</th>
-                                            <th>Actions</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($book_categories as $book_category)
-                                        <tr>
-                                            <td>
-                                                {{ $book_category->category_name }}
-                                            </td>
-                                            <td>
-                                                {{ $book_category->category_description }}
-                                            </td>
-                                            <td>
-                                                <a
-                                                    href="{{ route('book_categories.update', ['id' => $book_category->id]) }}"
-                                                    class="btn btn-primary"
-                                                    >Edit</a
-                                                >
-                                               
-                                                <form
-                                                    action="{{ route('book_categories.delete', ['id' => $book_category->id]) }}"
-                                                    method="POST"
-                                                    class="d-inline"
-                                                >
-                                                    @csrf @method('DELETE')
-                                                    <button
-                                                    type="submit"
-                                                    class="btn btn-danger"
-                                                    >
-                                                        Delete
-                                                    </button>
-                                                </form>
-                                            </td>
-                                            
-                                        </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                                <br />
-                                {{ $book_categories->links('pagination::bootstrap-5') }}
-                            </div>
-                        </div>
-                    </div>
-                    <!-- /.card -->
-                </div>
-                <!-- /.col -->
-            </div>
-            <!-- /.row (main row) -->
+          </div>
         </div>
+
+        <!-- Main row -->
+        <div class="row">
+          <div class="col-md-12">
+            <div class="container-fluid">
+              <div class="card">
+                <div class="table-responsive">
+                  <table class="table table-bordered">
+                    <thead>
+                      <tr>
+                        <th>Category Name</th>
+                        <th>Category Description</th>
+                        <th>Actions</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      @foreach ($book_categories as $book_category)
+                        <tr>
+                          <td>
+                            {{ $book_category->category_name }}
+                          </td>
+                          <td>
+                            {{ $book_category->category_description }}
+                          </td>
+                          <td>
+                            <a href="{{ route('book_categories.update', ['id' => $book_category->id]) }}"
+                              class="btn btn-primary">Edit</a>
+
+                            <form action="{{ route('book_categories.delete', ['id' => $book_category->id]) }}"
+                              method="POST" class="d-inline">
+                              @csrf @method('DELETE')
+                              <button type="submit" class="btn btn-danger">
+                                Delete
+                              </button>
+                            </form>
+                          </td>
+                        </tr>
+                      @endforeach
+                    </tbody>
+                  </table>
+                  <br />
+                  {{ $book_categories->links('pagination::bootstrap-5') }}
+                </div>
+              </div>
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row (main row) -->
+      </div>
     </div>
-</div>
+  </div>
 @endsection

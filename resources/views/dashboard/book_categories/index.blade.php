76c0a00ab20f820b7369d79d65b638bae9931095
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-02-01 10:01:33 +0700

    add: confirmation box (delete category)

--- resources/views/dashboard/book_categories/index.blade.php
+++ resources/views/dashboard/book_categories/index.blade.php
@@ -77,9 +77,36 @@
                             <form action="{{ route('book_categories.delete', ['id' => $book_category->id]) }}"
                               method="POST" class="d-inline">
                               @csrf @method('DELETE')
-                              <button type="submit" class="btn btn-danger">
+                              <button type="button" class="btn btn-danger" data-toggle="modal"
+                                data-target="#confirmationModal">
                                 Delete
                               </button>
+
+                              <!-- Confirmation alert modal -->
+                              <div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog"
+                                aria-labelledby="confirmationModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                  <div class="modal-content">
+                                    <div class="modal-header">
+                                      <h4 class="modal-title" id="confirmationModalLabel">Confirmation</h4>
+                                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                      </button>
+                                    </div>
+                                    <div class="modal-body">
+                                      <p>Are you sure you want to delete this category?</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                      <button type="button" class="btn btn-default" data-dismiss="modal">
+                                        Close
+                                      </button>
+                                      <button type="submit" class="btn btn-danger">
+                                        Delete
+                                      </button>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
                             </form>
                           </td>
                         </tr>

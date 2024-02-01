d7b340f4209c652184a5faf0d07b827abeaa92d9
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-02-01 09:59:00 +0700

    add: modal confirmation box (update user) (update profile) (create book category)

--- resources/views/dashboard/book_categories/create.blade.php
+++ resources/views/dashboard/book_categories/create.blade.php
@@ -42,9 +42,35 @@
                 <label for="author" class="form-label">Category Description:</label>
                 <textarea name="category_description" class="form-control">{{ old('category_description') }}</textarea>
 
-                <button type="submit" class="btn btn-primary mt-2">
-                  Create Book Category
+                <button type="button" class="btn btn-primary mt-2" data-toggle="modal" data-target="#confirmationModal">
+                  Create Category
                 </button>
+    
+                <!-- Confirmation alert modal -->
+                <div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog"
+                    aria-labelledby="confirmationModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title" id="confirmationModalLabel">Confirmation</h4>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <p>Are you sure you want to add this category?</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">
+                                    Close
+                                </button>
+                                <button type="submit" class="btn btn-primary">
+                                    Create Category
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
               </form>
             </div>
           </div>
